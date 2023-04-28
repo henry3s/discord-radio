e0db6038d11516e413320292f211b7915ec0b617
henry3s <henrys.homenz@gmail.com>
2023-04-28 17:38:29 +1200

    more robust

--- radio.py
+++ radio.py
@@ -9,9 +9,8 @@ intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
 
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-MP3_FILE = "<PATH/TO/MP3>"
+TOKEN = "<YOUR TOKEN HERE>"
+MP3_FILE = "<PATH/TO/MP3"
 
 FFMPEG_OPTIONS = {
     "options": "-vn", 
@@ -32,7 +31,12 @@ async def connect_and_play(voice_channel):
                 await asyncio.sleep(1)
             print(f"Disconnected from voice channel: {voice_channel.name} ({voice_channel.id})")
             voice_client.stop()
-            await voice_client.cleanup()
+            if voice_client and voice_client.is_connected():
+                await voice_client.cleanup()
+
+            # Reconnect if the bot is not in a voice channel in the same guild or still connecting
+            if voice_channel.guild.voice_client is None or voice_channel.guild.voice_client.is_connecting():
+                await connect_and_play(voice_channel)
 
         voice_client.play(audio_source)
         asyncio.create_task(on_disconnect())
@@ -51,7 +55,6 @@ async def connect_and_play(voice_channel):
 async def on_voice_state_update(member, before, after):
     if member.guild_permissions.administrator:
         if before.channel is None and after.channel is not None:
-            # Check if the bot is already connected to a voice channel in this server
             if after.channel.guild.voice_client is not None:
                 return
 
@@ -60,18 +63,4 @@ async def on_voice_state_update(member, before, after):
                 task = asyncio.create_task(connect_and_play(after.channel))
                 connection_tasks[after.channel.guild.id] = task
 
-# Shutdown command - used mainly for quicker debugging
-@bot.command(name='shutdown', help='Shut down the bot (Admin only)')
-@commands.has_permissions(administrator=True)
-async def shutdown(ctx):
-    await ctx.send('Shutting down...')
-    
-    global connection_tasks
-    for task in connection_tasks.values():
-        task.cancel()
-    
-    await bot.logout()
-
-
-bot.run("<YOUR TOKEN ID>")
-
+bot.run(TOKEN)
